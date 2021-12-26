dc8b6211467a145c9564a6b043039d2f1e1ea522
LuanRLima <luanrlima9@gmail.com>
2021-12-26 09:17:40 -0300

    :tada: My fisrt commit

---
+++ src/test/java/one/digitalinnovation/Beer/BeerApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.Beer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
